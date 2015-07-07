cf39078eca0135b98c70e72e4d0090aaafb260d3
hs <hshipper@users.noreply.github.com>
2015-07-06 23:16:11 -0700

    added curly braces to make png command work better

--- plot2.R
+++ plot2.R
@@ -6,9 +6,10 @@
 
 pwr <- read.table("shortened_power.txt", header = TRUE, sep = ";")
 
-plot(pwr$Global_active_power, type = "l",
-     xaxt = 'n', ylab = "Global Active Power (kilowatts)")
-axis(1, at=c(0, 1441, 2881), labels = c("Thu", "Fri", "Sat"))
+plot <- {plot(pwr$Global_active_power, type = "l",
+          xaxt = 'n', ylab = "Global Active Power (kilowatts)", xlab = "")
+          axis(1, at=c(0, 1441, 2881), labels = c("Thu", "Fri", "Sat"))
+}
 
 png(filename = "plot2.png", width = 480, height = 480)
 
