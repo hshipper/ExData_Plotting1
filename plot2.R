955c8d8d500b8bf99b38332af278bd6242c7699c
hs <hshipper@users.noreply.github.com>
2015-07-06 23:12:22 -0700

    This actually works.

--- plot2.R
+++ plot2.R
@@ -4,19 +4,12 @@
 ###range of dates. Shell code to perform that operation is included in
 ###README.md
 
-library(lubridate)
-library(tidyr)
-library(dplyr)
-
 pwr <- read.table("shortened_power.txt", header = TRUE, sep = ";")
-power <- tbl_df(pwr)
 
-### The following code uses dplyr and tidyr functions to merge Date and Time
-power <- power %>%
-          unite(Date_Time, Date, Time, sep = " ") %>%
-          mutate(Date_Time = fast_strptime(Date_Time, "%d/%m/%Y %H:%M:%S"))
+plot(pwr$Global_active_power, type = "l",
+     xaxt = 'n', ylab = "Global Active Power (kilowatts)")
+axis(1, at=c(0, 1441, 2881), labels = c("Thu", "Fri", "Sat"))
 
-power %>%
-  select(Date_Time, Global_active_power) %>%
-  plot(Global_active_power ~ Date_Time, .)
+png(filename = "plot2.png", width = 480, height = 480)
+
 
